a8a87696a6d6d911fb06200546b23569c4ebb880
Bhushannasre <bhushannasre3@gmail.com>
2024-01-24 22:44:31 +0530

    Basic

---
+++ Ascii.cpp
@@ -0,0 +1,13 @@
+/*Accept character and find out ascii value*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    char value;
+
+    cout<<"Enter any character:-";
+    cin>>value;
+    cout<<"ASCII="<<int(value);
+    return 0;
+
+}
