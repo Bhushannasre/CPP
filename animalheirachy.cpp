7b5cd06b4363c9b944d052bb895462b0879edaff
Bhushannasre <bhushannasre3@gmail.com>
2024-01-25 23:54:16 +0530

    Interitance animal

---
+++ animalheirachy.cpp
@@ -0,0 +1,4 @@
+/*Animal Hierarchy:Problem Statement: Create a hierarchy of animal classes. Start with a base class Animal and
+then create derived classes like Mammal, Bird, and Fish. Each of these derived classes should have specific properties
+and methods related to their respective categories of animals.*/
+
